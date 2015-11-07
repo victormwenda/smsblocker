445592f6aa7fe27e21da51c7bd3c22ec9e56647f
Victor Mwenda <vmwenda.vm@gmail.com>
2015-11-07 16:52:22 +0300

    Added SQLiteOpenHelper for Database Management

--- app/src/main/java/com/marvik/apps/smsblocker/database/schemas/Database.java
+++ app/src/main/java/com/marvik/apps/smsblocker/database/schemas/Database.java
@@ -1,7 +1,41 @@
 package com.marvik.apps.smsblocker.database.schemas;
 
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
 /**
  * Created by victor on 11/7/2015.
  */
-public class Database {
+public class Database extends SQLiteOpenHelper {
+
+    private static final String DATABASE_NAME = "smsblocker";
+    private static final int DATABASE_VERSION = 1;
+    private static final SQLiteDatabase.CursorFactory CURSOR_FACTORY = null;
+
+    public Database(Context context) {
+        super(context, DATABASE_NAME, CURSOR_FACTORY, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        createTables(db);
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        createTables(db);
+    }
+
+    private void createTables(SQLiteDatabase db) {
+        String[] tables_sql = Tables.TablesSQL.SQL;
+        for (String sql : tables_sql) {
+            createTable(db, sql);
+        }
+    }
+
+    private void createTable(SQLiteDatabase db, String sql) {
+        db.execSQL(sql);
+    }
 }
--- app/src/main/java/com/marvik/apps/smsblocker/database/schemas/Tables.java
+++ app/src/main/java/com/marvik/apps/smsblocker/database/schemas/Tables.java
@@ -1,9 +1,15 @@
 package com.marvik.apps.smsblocker.database.schemas;
 
+import android.net.Uri;
+
 /**
  * Created by victor on 11/7/2015.
  */
 public class Tables {
+
+    public static class TablesSQL {
+        public static final String[] SQL = {BlockedSms.SQL};
+    }
 
     public static class BlockedSms {
 
@@ -14,5 +20,21 @@ public class Tables {
         public static final String COL_MESSAGE_RECEIVE_TIME = "receive_time";
         public static final String COL_MESSAGE_SYSTEM_TIME = "system_time";
 
+        public static final String TABLE_NAME = "blockedsms";
+
+        public static final Uri CONTENT_URI = Uri.parse("content://");
+
+        public static final String SQL = "CREATE TABLE IF NOT EXISTS " + TABLE_NAME
+                + "("
+                + COL_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
+                + COL_SENDER_PHONENUMBER + " TEXT NOT NULL, "
+                + COL_MESSAGE_TEXT + " TEXT, "
+                + COL_MESSAGE_SEND_TIME + " LONG NOT NULL, "
+                + COL_MESSAGE_RECEIVE_TIME + " LONG NOT NULL, "
+                + COL_MESSAGE_SYSTEM_TIME + " LONG NOT NULL, "
+                + ");";
+
     }
+
+
 }
