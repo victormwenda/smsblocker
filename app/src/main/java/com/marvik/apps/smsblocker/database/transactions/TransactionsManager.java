e34c173b388b365dd30322a47738604cf5bafd21
Victor Mwenda <vmwenda.vm@gmail.com>
2015-11-07 16:39:02 +0300

    Added A few Constructors

--- app/src/main/java/com/marvik/apps/smsblocker/database/transactions/TransactionsManager.java
+++ app/src/main/java/com/marvik/apps/smsblocker/database/transactions/TransactionsManager.java
@@ -1,7 +1,19 @@
 package com.marvik.apps.smsblocker.database.transactions;
 
+import android.content.Context;
+
 /**
  * Created by victor on 11/7/2015.
  */
 public class TransactionsManager {
+
+    private Context context;
+
+    public TransactionsManager(Context context) {
+        this.context = context;
+    }
+
+    public Context getContext() {
+        return context;
+    }
 }
--- app/src/main/java/com/marvik/apps/smsblocker/utils/Utils.java
+++ app/src/main/java/com/marvik/apps/smsblocker/utils/Utils.java
@@ -3,6 +3,7 @@ package com.marvik.apps.smsblocker.utils;
 import android.content.Context;
 
 import com.marvik.apis.core.utilities.Utilities;
+import com.marvik.apps.smsblocker.database.transactions.TransactionsManager;
 
 /**
  * Created by victor on 11/7/2015.
@@ -12,5 +13,28 @@ public class Utils {
     private Context context;
 
     private Utilities utilities;
+    private TransactionsManager transactionsManager;
 
+    public Utils(Context context) {
+        initAll(context);
+    }
+
+    private void initAll(Context context) {
+        this.context = context;
+
+        utilities = new Utilities(getContext());
+        transactionsManager = new TransactionsManager(getContext());
+    }
+
+    public Context getContext() {
+        return context;
+    }
+
+    public Utilities getUtilities() {
+        return utilities;
+    }
+
+    public TransactionsManager getTransactionsManager() {
+        return transactionsManager;
+    }
 }
